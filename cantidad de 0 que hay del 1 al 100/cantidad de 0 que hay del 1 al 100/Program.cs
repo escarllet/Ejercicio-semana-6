b1e6e0f14e293d5d9fd62b886e29acfff407194e
Escarllet Suriel Valdez <Escarlletsurielvaldez@hotmail.com>
2020-06-14 16:51:30 -0500

    Muestre la cantidad de 0 que hay del 1 al 100. version 1.0

---
+++ cantidad de 0 que hay del 1 al 100/cantidad de 0 que hay del 1 al 100/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cantidad_de_0_que_hay_del_1_al_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int con1=0, con2 =0;
+            for (int i = 1; i <=100; i++)
+            {
+                if (i%10 == 0 && i<100 )
+                {
+                    con1+= 1;
+                }
+                else if (i % 10 == 0 && i >= 100)
+                {
+                    con1 += 2;
+                }
+            }
+            int a = con1 + con2;
+            Console.WriteLine("la cantidad de ceros que hay del 1 al 100 es: "+a);
+            Console.ReadKey();
+        }
+    }
+}
