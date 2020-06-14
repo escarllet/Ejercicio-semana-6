7a7892b78aefcd8c89d29e386ae75bb09402c945
Escarllet Suriel Valdez <Escarlletsurielvaldez@hotmail.com>
2020-06-14 15:24:32 -0500

    Cantidad de digitos de un numero positivo. version 1.0

---
+++ cantidad de digitos/cantidad de digitos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cantidad_de_digitos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Digite un numero positivo");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a>=0)
+            {
+                int f;
+                string b;
+                b = Convert.ToString(a);
+                f = b.Length;
+                Console.WriteLine("Su numero tiene "+f+ " digitos");
+            }
+            else
+            {
+                Console.WriteLine("Error: no ha digitado un numero o el numero no es positivo");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
