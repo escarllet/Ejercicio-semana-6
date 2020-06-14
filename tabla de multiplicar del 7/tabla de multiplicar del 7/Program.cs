7703be1200d9925240c5e5ec48e4f77ac8f023c9
Escarllet Suriel Valdez <Escarlletsurielvaldez@hotmail.com>
2020-06-14 15:04:15 -0500

    tabla de multiplicar del 7 usando while. version 1.0

---
+++ tabla de multiplicar del 7/tabla de multiplicar del 7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabla_de_multiplicar_del_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0,a;
+            while (i<=12)
+            {
+                a = 7 * i;
+                Console.WriteLine("7 x "+i+" = "+a);
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
