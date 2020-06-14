7a0e687ba332be2586ad242b85d884471f779da3
Escarllet Suriel Valdez <Escarlletsurielvaldez@hotmail.com>
2020-06-14 15:43:17 -0500

    Recorrer los numeros del 1 al 50 en un ciclo for. version 1.0

---
+++ for del 1 al 50/for del 1 al 50/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_del_1_al_50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
