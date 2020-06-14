2b456bf8f8ed99ce5921fc8822df579172955bac
Escarllet Suriel Valdez <Escarlletsurielvaldez@hotmail.com>
2020-06-14 16:03:54 -0500

    Numero aleatorio del 50 al 100, termina si escribe S. version 1.0

---
+++ Numero aleatorio de 50 a 100/Numero aleatorio de 50 a 100/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numero_aleatorio_de_50_a_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            do
+            {
+                Random rand = new Random();
+                int numero = rand.Next(50, 100);
+                Console.WriteLine("Se ha generado un numero aleatorio, este numero es " + numero);
+                Console.WriteLine("Desea terminar el programa? (S/N)");
+                a = Console.ReadLine();
+
+            } while (a != "S");
+            Console.ReadKey();
+        }
+    }
+}
